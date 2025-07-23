358c2049181033f92fedf3ce89c5ffaaf78e8a1b
Nikita <86563787+Tsutsurin@users.noreply.github.com>
2025-07-23 20:14:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.resolve()
 
-# Пути к директориям
 DRIVERS_DIR = BASE_DIR / 'drivers'
 LOGS_DIR = BASE_DIR / 'logs'
 RESULTS_DIR = BASE_DIR / 'results'
 
-# Создание директорий при необходимости
 for directory in [DRIVERS_DIR, LOGS_DIR, RESULTS_DIR]:
     directory.mkdir(exist_ok=True)
